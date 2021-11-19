3097669bf2c2ecbe8a7a86f6e9a9c40cd576d918
Orlando Chaparro <orlancss1@gmail.com>
2021-11-19 14:51:31 -0400

    Arreglado bug de la importacion de ./routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/index.css";
-import Routes from "./Routes";
+import Routes from "./routes";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
